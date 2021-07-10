c098e487d15c3bef0b4a0abbfd1e0d1c25e497b8
Anthony Parrino <anthony21parrino21@gmail.com>
2021-07-09 21:05:06 -0400

    created screen files and added external fonts function

--- App.js
+++ App.js
@@ -1,21 +1,42 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+// importing fonts from asset folder-- npm i --save expo-font
+import * as Font from 'expo-font';
+// npm i expo-app-loading, also for external fonts
+import AppLoading from 'expo-app-loading';
+
+// function to fetch the fonts
+const fetchFonts = () => {
+  return Font.loadAsync({
+    'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
+    'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf')
+  })
+}
+
 export default function App() {
+
+  // state to handle the font loading
+  const [fontLoaded, setFontLoaded] = useState(false)
+
+  // if check to make sure font is loaded
+  if (!fontLoaded) {
+    return <AppLoading
+      startAsync={fetchFonts}
+      onFinish={() => setFontLoaded(true)}
+      onError={(err) => console.log(err)}
+    />
+  }
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+
   },
 });
