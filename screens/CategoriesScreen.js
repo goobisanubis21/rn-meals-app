991ec40da28b025dede1ee16dffb029ba80b3221
Anthony Parrino <anthony21parrino21@gmail.com>
2021-07-11 15:05:34 -0400

    passing data from one screen to another

--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -8,7 +8,10 @@ const CategoriesScreen = (props) => {
 
     const renderGridItem = (itemData) => {
         return (
-            <TouchableOpacity style={styles.gridItem} onPress={() => {props.navigation.navigate('CategoryMeals')} }>
+            // to send data from one page to the next use params within the .natigate({})
+            <TouchableOpacity style={styles.gridItem} onPress={() => {props.navigation.navigate({routeName: 'CategoryMeals', params: {
+                categoryId: itemData.item.id
+            }})} }>
                 <View>
                     <Text>
                         {itemData.item.title}
--- screens/CategoryMealsScreen.js
+++ screens/CategoryMealsScreen.js
@@ -1,10 +1,17 @@
 import React from 'react'
 import { Button, StyleSheet, Text, View } from 'react-native'
+import { CATEGORIES } from '../data/dummy-data'
 
 const CategoryMealsScreen = (props) => {
+
+    // to get the data from the naviagte({}) use getParam
+    const catId= props.navigation.getParam('categoryId')
+    const selectedCategory = CATEGORIES.find(cat => cat.id === catId)
+
     return (
         <View style={styles.screen}>
             <Text>Category Meal Screen</Text>
+            <Text>{selectedCategory.title}</Text>
 
             <Button title='Go to Details'
                 onPress={() => {
