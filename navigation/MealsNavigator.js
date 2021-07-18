3f9714ab5e36d32db126a7f9688a6d2b098f67f0
Anthony Parrino <anthony21parrino21@gmail.com>
2021-07-18 12:50:25 -0400

    creating favorites

--- navigation/MealsNavigator.js
+++ navigation/MealsNavigator.js
@@ -1,11 +1,15 @@
+import React from 'react';
+
 // createAppContainer is used for switching between screens
-import { createAppContainer} from 'react-navigation';
+import { createAppContainer } from 'react-navigation';
 
 // createBottomTabNavigator is needed for the bottom tabs using npm install react-navigation-tabs
-import {createBottomTabNavigator } from 'react-navigation-tabs'
+import { createBottomTabNavigator } from 'react-navigation-tabs'
 
 // needed for stack navigator - npm install --save react-navigation-stack
 import { createStackNavigator } from 'react-navigation-stack';
+
+import { Ionicons } from '@expo/vector-icons';
 
 // import all the screens for the navigator fucntion
 import CatergoriesScreen from '../screens/CategoriesScreen';
@@ -13,6 +17,7 @@ import CatergoryMealsScreen from '../screens/CategoryMealsScreen';
 import MealDetailScreen from '../screens/MealDetailScreen';
 import Colors from '../constants/Colors';
 import FavoritesScreen from '../screens/FavoritesScreen'
+import Meal from '../models/meal';
 
 // function from the create stack import to tell App that these are the screens we are able to move between along with the navigation options which gives the header styling
 const MealsNavigator = createStackNavigator({
@@ -58,11 +63,45 @@ const MealsNavigator = createStackNavigator({
     // }
 );
 
-// const for adding the tabs to the bottom of the app
+
+// stack for the favorites page
+const FavNavigator = createStackNavigator({
+    Favorites: FavoritesScreen,
+    MealDetail: MealDetailScreen
+}, {
+    defaultNavigationOptions: {
+        headerStyle: {
+            backgroundColor: Platform.OS === 'android' ? Colors.primaryColor : 'white'
+        },
+        headerTintColor: Platform.OS === 'android' ? 'white' : Colors.primaryColor
+    }
+});
+
+
+
+// const for adding the tabs to the bottom of the app in the first obj, the second arg object is for styling the tabs
 const MealsFavTabNavigator = createBottomTabNavigator({
-    Meals: MealsNavigator,
-    Favorites: FavoritesScreen
-})
+    Meals: {
+        screen: MealsNavigator,
+        navigationOptions: {
+            tabBarIcon: (tabInfo) => {
+                return <Ionicons name='ios-restaurant' size={25} color={tabInfo.tintColor} />
+            }
+        }
+    },
+    Favorites: {
+        screen: FavNavigator,
+        navigationOptions: {
+            tabBarIcon: (tabInfo) => {
+                return <Ionicons name='ios-star' size={25} color={tabInfo.tintColor} />
+            }
+        }
+    }
+}, {
+    tabBarOptions: {
+        activeTintColor: 'orange'
+    }
+});
 
 // exporting the MealsFavTabNavigator because the MealsNavigator const is nested within it
 export default createAppContainer(MealsFavTabNavigator)
--- screens/CategoryMealsScreen.js
+++ screens/CategoryMealsScreen.js
@@ -1,26 +1,9 @@
 import React from 'react'
-import { FlatList, StyleSheet, Text, View } from 'react-native'
 import { CATEGORIES, MEALS } from '../data/dummy-data'
-import MealItem from '../components/MealItem'
-
+import MealList from '../components/MealList'
 const CategoryMealsScreen = (props) => {
 
-    const renderMealItem = (itemData) => {
-        return (
-            <MealItem 
-                title={itemData.item.title}
-                image={itemData.item.imageUrl}
-                duration={itemData.item.duration}
-                complexity={itemData.item.complexity}
-                affordability={itemData.item.affordability}
-                onSelectMeal={() => {
-                    props.navigation.navigate({routeName: 'MealDetail', params: {
-                        mealId: itemData.item.id
-                    }})
-                }}
-            />
-        )
-    }
+    
 
     // to get the data from the naviagte({}) use getParam
     const catId = props.navigation.getParam('categoryId')
@@ -29,14 +12,7 @@ const CategoryMealsScreen = (props) => {
     const displayedMeals = MEALS.filter(meal => meal.categoryIds.indexOf(catId) >= 0)
 
     return (
-        <View style={styles.screen}>
-            <FlatList 
-                data={displayedMeals} 
-                keyExtractor={(item, index) => item.id} 
-                renderItem={renderMealItem} 
-                style={{width:'100%'}}
-            />
-        </View>
+        <MealList listData={displayedMeals} navigation={props.navigation} />
     )
 }
 
@@ -54,11 +30,3 @@ CategoryMealsScreen.navigationOptions = (navigationData) => {
 }
 
 export default CategoryMealsScreen
-
-const styles = StyleSheet.create({
-    screen: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center'
-    }
-})
